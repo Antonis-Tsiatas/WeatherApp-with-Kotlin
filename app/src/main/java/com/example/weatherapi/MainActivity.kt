f1e834a97301040885ee76274a1efbc3a590619a
Antonis Tsiatas <151565585+Antonis-Tsiatas@users.noreply.github.com>
2024-01-29 17:36:35 +0200

    Added error handling for location permissions and humidity

--- app/src/main/java/com/example/weatherapi/MainActivity.kt
+++ app/src/main/java/com/example/weatherapi/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.weatherapi
 
+import android.Manifest
 import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
@@ -7,6 +8,7 @@ import android.location.Location
 import android.location.LocationManager
 import android.os.Bundle
 import android.provider.Settings
+import android.util.Log
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -14,15 +16,12 @@ import androidx.core.app.ActivityCompat
 import com.bumptech.glide.Glide
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
-import com.google.android.gms.tasks.Task
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import android.Manifest
-import android.util.Log
 
 class MainActivity : AppCompatActivity() {
 
@@ -47,6 +46,10 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    private fun showError(message: String) {
+        Toast.makeText(this, "Error: $message", Toast.LENGTH_SHORT).show()
+    }
+
     private fun checkLocationPermission(): Boolean {
         return if (ActivityCompat.checkSelfPermission(
                 this,
@@ -55,6 +58,7 @@ class MainActivity : AppCompatActivity() {
         ) {
             true
         } else {
+            showError("Location permission not granted")
             ActivityCompat.requestPermissions(
                 this,
                 arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
@@ -67,10 +71,8 @@ class MainActivity : AppCompatActivity() {
     private fun requestLocation() {
         val locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
         if (!locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
-            // Εάν το GPS δεν είναι ενεργοποιημένο, προτρέπουμε τον χρήστη να το ενεργοποιήσει
             startActivity(Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS))
         } else {
-            // Αν το GPS είναι ενεργοποιημένο, ζητάμε την τοποθεσία του χρήστη
             if (ActivityCompat.checkSelfPermission(
                     this,
                     Manifest.permission.ACCESS_FINE_LOCATION
@@ -79,40 +81,33 @@ class MainActivity : AppCompatActivity() {
                     Manifest.permission.ACCESS_COARSE_LOCATION
                 ) != PackageManager.PERMISSION_GRANTED
             ) {
-                // TODO: Consider calling
-                //    ActivityCompat#requestPermissions
-                // here to request the missing permissions, and then overriding
-                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                //                                          int[] grantResults)
-                // to handle the case where the user grants the permission. See the documentation
-                // for ActivityCompat#requestPermissions for more details.
-                return
-            }
-            fusedLocationClient.lastLocation.addOnSuccessListener { location: Location? ->
-                location?.let {
-                    val latitude = it.latitude
-                    val longitude = it.longitude
-                    Log.d("WeatherApp", "User location - Latitude: $latitude, Longitude: $longitude")
-                    Log.d("WeatherApp", "User location - Latitude: $latitude, Longitude: $longitude")
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                    LOCATION_PERMISSION_REQUEST_CODE
+                )
+            } else {
+                fusedLocationClient.lastLocation.addOnSuccessListener { location: Location? ->
+                    location?.let {
+                        val latitude = it.latitude
+                        val longitude = it.longitude
+                        Log.d("WeatherApp", "User location - Latitude: $latitude, Longitude: $longitude")
 
-                    GlobalScope.launch(Dispatchers.IO) {
-                        try {
-                            val weatherData =
-                                weatherService.getWeatherByCoordinates(
-                                    latitude.toString(),
-                                    longitude.toString(),
-                                    apiKey
-                                )
-                            withContext(Dispatchers.Main) {
-                                updateUI(weatherData)
-                            }
-                        } catch (e: Exception) {
-                            withContext(Dispatchers.Main) {
-                                Toast.makeText(
-                                    this@MainActivity,
-                                    "Error loading weather data",
-                                    Toast.LENGTH_SHORT
-                                ).show()
+                        GlobalScope.launch(Dispatchers.IO) {
+                            try {
+                                val weatherData =
+                                    weatherService.getWeatherByCoordinates(
+                                        latitude.toString(),
+                                        longitude.toString(),
+                                        apiKey
+                                    )
+                                withContext(Dispatchers.Main) {
+                                    updateUI(weatherData)
+                                }
+                            } catch (e: Exception) {
+                                withContext(Dispatchers.Main) {
+                                    showError("Error loading weather data")
+                                }
                             }
                         }
                     }
@@ -129,6 +124,9 @@ class MainActivity : AppCompatActivity() {
         Glide.with(this)
             .load(iconUrl)
             .into(findViewById(R.id.imageViewWeatherIcon))
+
+        findViewById<TextView>(R.id.textViewHumidityWind).text =
+            "Humidity: ${weatherData.main?.humidity ?: "Unknown"}% | Wind Speed: ${weatherData.wind?.speed ?: "Unknown"} m/s"
     }
 
     companion object {
@@ -138,14 +136,20 @@ class MainActivity : AppCompatActivity() {
     data class WeatherData(
         val name: String,
         val main: Main,
-        val weather: List<Weather>
+        val weather: List<Weather>,
+        val wind: Wind
     )
 
     data class Main(
-        val temp: Double
+        val temp: Double,
+        val humidity: Int // Προσθέστε το πεδίο υγρασίας εδώ
     )
 
     data class Weather(
         val icon: String
+    )
+
+    data class Wind(
+        val speed: Double
     )
 }
